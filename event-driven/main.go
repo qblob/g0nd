b4369b5311b1b9ca46b85d2abf46376bac4e4f94
AR <nandlab@outlook.com>
2025-01-05 12:23:25 +0330

    Added the basic event-driven service

--- event-driven/main.go
+++ event-driven/main.go
@@ -4,11 +4,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
+var eventChannel = make(chan string)
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	// log.Println("Handler called")
-	// fmt.Fprintf(w, "<html><body><h1>Welcome to my website!</h1></body></html>")
 	htmlContent, err := os.ReadFile("./index.html")
 	if err != nil {
 		log.Printf("Error reading HTML file: %v", err)
@@ -19,11 +20,25 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.Write(htmlContent)
 
+	// Send an event to the channel
+	eventChannel <- "Page accessed"
+}
+
+func eventListener() {
+	for {
+		select {
+		case event := <-eventChannel:
+			log.Printf("Event received: %s", event)
+		case <-time.After(10 * time.Second):
+			log.Println("No events received in the last 10 seconds")
+		}
+	}
 }
 
 func main() {
+	go eventListener()
 	http.HandleFunc("/", handler)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
 }
